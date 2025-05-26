75065675dc6286ccd188d40cecb895cad5317c50
Lhianee <v-schulz@gmx.net>
2025-05-26 18:33:05 +0200

    Limitierung auf 60 FPS eingebaut

--- main.py
+++ main.py
@@ -2,16 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+# Import der Spielkonstanten
 from constants import *
+# Clock object
+clock = pygame.time.Clock()
+# delta time variable
+dt = 0
 
 def main():
     pygame.init()
+    # Bildschirmfläche wird festgelegt
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
+    #Game Loop beginnt hier
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # schwarzer Hintergrund    
         screen.fill((0,0,0), rect=None, special_flags=0)
         pygame.display.flip()
+        # Warten für 1/60 Sekunde --> Limit auf 60FPS und Speichern der dt nach jedem Tick 
+        dt = clock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
